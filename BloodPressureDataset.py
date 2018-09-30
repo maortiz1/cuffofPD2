02b1450334e37fd82481cf7c2d747bddccb23355
Cindy Hernandez <ca.hernandez14@uniandes.edu.co>
2018-09-29 22:59:49 -0500

    usando Pan-Tompkins en ECG

--- BloodPressureDataset.py
+++ BloodPressureDataset.py
@@ -8,65 +8,55 @@ import pickle
 
 ##############################################################
 cwd = os.getcwd()
-#Get the names of the .csv file
 path_samples_csv= os.path.join(cwd,'mat_to_csv')
 files_csv = [f for f in listdir(path_samples_csv) if isfile(join(path_samples_csv, f))]
-#read the files
 os.chdir(path_samples_csv)
 #############################################################
 fr = 125
 
-data_train_name = []
-data_train_PPG  = np.empty(1)
-data_train_ABP  = np.empty(1)
-data_train_ECG  = np.empty(1)
 t_RRtrain = []
 
-data_test_name = []
-data_test_PPG  = np.empty(1)
-data_test_ABP  = np.empty(1)
-data_test_ECG  = np.empty(1)
 t_RRtest = []
 ######################################################################
 def fun_Pan_Tompkins(t, ECG):
-	dx = (ECG[2:] - ECG[0:-2])/(t[2:]-t[0:-2])
-	dx = dx * dx
-	w_movil = np.zeros(len(dx))
-	w_size = 20
-	for i in range(len(dx)-w_size):
-		w_movil[i]=np.sum(dx[i:i+w_size])
-	peaks = find_peaks(w_movil, width = w_size, height= np.var(w_movil)**0.5)
-	t = t[peaks[0]]
-	return t
-#########################################################################
-for i in range(6500):
-    #print(files_csv[i])
-    Matrix_data = np.genfromtxt(files_csv[i], delimiter=",")
-    data_train_name =files_csv[i]
-    data_train_PPG = (Matrix_data[0,:])
-    data_train_ABP = (Matrix_data[1,:])
-    data_train_ECG = (Matrix_data[2,:])
-    t_train = np.arange(len(data_train_ECG))/fr
-    t_RRtrain.append(fun_Pan_Tompkins(t_train, data_train_ECG))       
+    """ Algoritmo de Pan-Tompkins para hallar RR
+    """
+    dx = (ECG[2:] - ECG[0:-2])/(t[2:]-t[0:-2])
+    dx = dx * dx
+    w_movil = np.zeros(len(dx))
+    w_size = 20
 
-for i in range(6500,9365):
-    #print(files_csv[i])
-    Matrix_data = np.genfromtxt(files_csv[i], delimiter=",")
-    data_test_name = files_csv[i]
-    data_test_PPG = (Matrix_data[0,:])
-    data_test_ABP = (Matrix_data[1,:])
-    data_test_ECG = (Matrix_data[2,:])
-    t_test = np.arange(len(data_test_ECG))/fr
-    t_RRtest.append(fun_Pan_Tompkins(t_test, data_test_ECG))
+    for i in range(len(dx)-w_size):
+	    w_movil[i]=np.sum(dx[i:i+w_size])
+
+    peaks = find_peaks(w_movil, width = w_size, height= np.var(w_movil)**0.5)
+    t = t[peaks[0]]
+
+    return t
+#########################################################################
+for i in range(6000):
+	print(i)
+	data_train_PPG, data_train_ABP, data_train_ECG = np.genfromtxt(files_csv[i], delimiter=",")
+	print(data_train_PPG[0],data_train_ABP[0], data_train_ECG[0])
+	print(files_csv[i])
+	t_train = np.arange(len(data_train_ECG))/fr
+	t_RRtrain.append(fun_Pan_Tompkins(t_train, data_train_ECG))       
+
+for i in range(6000,9000):
+	print(i)
+	data_test_PPG, data_test_ABP, data_test_ECG = np.genfromtxt(files_csv[i], delimiter=",")
+	t_test = np.arange(len(data_test_ECG))/fr
+	t_RRtest.append(fun_Pan_Tompkins(t_test, data_test_ECG))
+	
 
 plt.figure()
 plt.plot(t_train, data_train_PPG)
-plt.title('PPG train %s'%data_train_name)
+plt.title('PPG train')
 plt.show()
 #####################################################################
-with open('RR_train', 'wb') as fp:
+
+with open('RR_train.mat', 'wb') as fp:
     pickle.dump(t_RRtrain, fp)
 
-with open('RR_test', 'wb') as fp:
+with open('RR_test.mat', 'wb') as fp:
     pickle.dump(t_RRtest, fp)
-
