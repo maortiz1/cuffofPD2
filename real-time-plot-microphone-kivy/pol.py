7cb45ba5558c9b95d6a2e69cc1c60ab87a23397f
Cindy Hernández <ca.hernandez14@uniandes.edu.co>
2018-11-11 14:13:41 -0500

    regresion

--- real-time-plot-microphone-kivy/pol.py
+++ real-time-plot-microphone-kivy/pol.py
@@ -13,154 +13,245 @@ import numpy as np
 #from os.path import isfile, join
 #Plot
 import matplotlib.pylab as plt
+import seaborn as sns
 #Scale
-#from sklearn.preprocessing import scale
+from sklearn.preprocessing import scale
 #Find_peaks
 import scipy
 from scipy.signal import find_peaks, filtfilt
+from sklearn import linear_model
+
+#ML
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
+
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
 #Save variables
 #import pickle
 #Biosignals
 import neurokit as nk
 
 
-#Leer datos (numeros)
-datos_n = np.genfromtxt('Copia de Datos real.txt')
-#Leer datos (texto)
-datos_t = np.genfromtxt('Copia de Datos real.txt', dtype = str)
+##Leer datos (numeros)
+#datos_n = np.genfromtxt('Copia de Datos real.txt')
+##Leer datos (texto)
+#datos_t = np.genfromtxt('Copia de Datos real.txt', dtype = str)
+#
+#nombres = datos_t[::9,0]
+#t_HR  = []
+#t_PPT = []
+#t_DBP = []
+#t_SBP = []
+#
+#cortar = [150000, 50000, 22000, 105000, 115000, 46000, 64000, 43000, 60000, 70000, 80000, 88000, 72000, 5500, 22000, 46000, 13000, 29000, 65000]
+#
+#for k in range(nombres.shape[0]):
+#
+#    #Tener los datos de cada uno de los .txt
+#    idx, DBP, SBP = datos_n[2+k*9:9+k*9,0], datos_n[2+k*9:9+k*9,1], datos_n[2+k*9:9+k*9,2]
+#
+#    idx = np.cumsum(np.array(idx[::-1], dtype = int))*250
+##    plt.figure()
+##    plt.subplot(211)
+##    plt.title(nombres[k])
+#    ECG = np.genfromtxt('%s_ecg.txt'%nombres[k], delimiter = ',')
+##    plt.plot(ECG)
+#    ECG = ECG[cortar[k]:-1]
+#    
+##    plt.subplot(212)
+#    PPG = np.genfromtxt('%s_ppg.txt'%nombres[k], delimiter = ',')
+##    plt.title(cortar[k])
+##    plt.plot(PPG)
+#    PPG = PPG[cortar[k]:-1]
+#
+#    idx_a = 0
+#
+#    b_ecg, a_ecg = scipy.signal.butter(1,[0.04,0.92],'bandpass')
+#    b_ppg, a_ppg = scipy.signal.butter(1,[0.01,0.04],'bandpass')
+#
+#    
+#    cont = 0
+#    for j in idx:
+#        
+#         
+#        idx_d = j
+#
+#        ECG_aux = ECG[idx_a:idx_d]
+#        PPG_aux = PPG[idx_a:idx_d]
+#
+#        ECG_aux = filtfilt(b_ecg, a_ecg, ECG_aux, method='gust')
+#
+#        #PPG_aux = filtfilt(b_ppg, a_ppg, PPG_aux, method='gust')
+#
+#        t = (np.arange(len(ECG_aux))/250)
+#        try:
+#            idx_peaksECG = nk.bio_process(ecg = ECG_aux, sampling_rate=250)['ECG']['R_Peaks']
+#            idx_peaksECG = np.delete(idx_peaksECG, np.where(np.diff(t[idx_peaksECG])<0.4))
+#
+#            t_RR = t[idx_peaksECG]
+#
+#            HR = 60/np.diff(t_RR)
+#
+#            idx_peaksPPG = []
+#            idx_del = []
+#
+#            for i in range(len(idx_peaksECG)-1):
+#
+#                ran_0 = idx_peaksECG[i]
+#                ran_1 = idx_peaksECG[i+1]
+#
+#                PPG_peak = (find_peaks(PPG_aux[ran_0:ran_1], distance = ran_1-ran_0 )[0] + ran_0)
+#
+#                if PPG_peak.size>0:# and PPG_aux[int(PPG_peak[0])]>500:
+#                    idx_peaksPPG.append(int(PPG_peak[0]))
+#                else:
+#                    idx_del.append(i)
+#
+#            idx_peaksECG = np.delete(idx_peaksECG, np.array(idx_del))
+#            HR    = np.delete(HR, np.array(idx_del))
+#            t_RR1 =  np.delete(t_RR, np.array(idx_del)+1)
+#            t_RR2 =  np.delete(t_RR, np.array(idx_del))
+#            t_PPG = t[idx_peaksPPG]
+#
+##            plt.figure()
+##            plt.subplot(211)
+##            plt.plot(t,ECG_aux)
+##            plt.scatter(t_RR,ECG_aux[idx_peaksECG], c='r')
+##            plt.title(nombres[k])
+##            plt.subplot(212)
+##            plt.plot(t,PPG_aux,'g')
+##            plt.scatter(t_PPG,PPG_aux[idx_peaksPPG], c='y')
+#
+#            PPT1 = (t_PPG-t_RR2[:-1])
+#            PPT2 = (t_RR1[1: ]-t_PPG)
+#            try:
+#                t_HR.append(np.mean(HR))
+#
+#                if np.mean(PPT1)>np.mean(PPT2):
+#                    t_PPT.append(np.mean(PPT1))
+#                else:
+#                    t_PPT.append(np.mean(PPT2))
+#                    
+#                t_DBP.append(DBP[cont])
+#                t_SBP.append(SBP[cont])
+#                cont +=1
+#    #            plt.figure()
+#    #            plt.plot(PPT1,'gx',label='PPG peak - previous RR peak ')
+#    #            plt.plot(PPT2,'rx',label='subsequent RR peak - PPG peak')
+#    #            plt.legend()
+#            except:
+#                print('error dato %s'%nombres[k])
+#                cont +=1
+##                t_HR.append([])
+##                t_PPT.append([])
+#
+#        except:
+#            print('error dato %s %d'%(nombres[k], cont+1))
+##            t_HR.append([])
+##            t_PPT.append([])
+#
+#        idx_a = j
+#
 
-nombres = datos_t[::9,0]
-t_HR  = []
-t_PPT = []
+HR_norm     = scale(t_HR)
+logPPT_norm = scale(np.log(t_PPT))
+DPB_norm    = scale(t_DBP)     
+SBP_norm    = scale(t_SBP)
 
-for k in range(nombres.shape[0]):
-
-    #Tener los datos de cada uno de los .txt
-    t_HR_cu  = []
-    t_PPT_cu = []
-    idx, DBP, SBP = datos_n[2+k*9:9+k*9,0], datos_n[2+k*9:9+k*9,1], datos_n[2+k*9:9+k*9,2]
-
-    idx = np.cumsum(np.array(idx[::-1], dtype = int))*250
-
-    ECG = np.genfromtxt('%s_ecg_copy.txt'%nombres[k], delimiter = ',')
-
-    PPG = np.genfromtxt('%s_ppg_copy.txt'%nombres[k], delimiter = ',')
-
-    idx_a = 0
-
-    b_ecg, a_ecg = scipy.signal.butter(1,[0.04,0.92],'bandpass')
-    b_ppg, a_ppg = scipy.signal.butter(1,[0.01,0.04],'bandpass')
-
-
-    for j in idx:
-        idx_d = j
-
-        ECG_aux = ECG[idx_a:idx_d]
-        PPG_aux = PPG[idx_a:idx_d]
-
-        ECG_aux = filtfilt(b_ecg, a_ecg, ECG_aux, method='gust')
-
-        PPG_aux = filtfilt(b_ppg, a_ppg, PPG_aux, method='gust')
-
-        t = (np.arange(len(ECG_aux))/250)
-        try:
-            idx_peaksECG = nk.bio_process(ecg = ECG_aux, sampling_rate=250)['ECG']['R_Peaks']
-            idx_peaksECG = np.delete(idx_peaksECG, np.where(np.diff(t[idx_peaksECG])<0.4))
-
-            t_RR = t[idx_peaksECG]
-
-            HR = 60/np.diff(t_RR)
-
-            idx_peaksPPG = []
-            idx_del = []
-
-            for i in range(len(idx_peaksECG)-1):
-
-                ran_0 = idx_peaksECG[i]
-                ran_1 = idx_peaksECG[i+1]
-
-                PPG_peak = (find_peaks(PPG_aux[ran_0:ran_1], distance = ran_1-ran_0 )[0] + ran_0)
-
-                if PPG_peak.size>0: #and PPG_aux[int(PPG_peak[0])]>500:
-                    idx_peaksPPG.append(int(PPG_peak[0]))
-                else:
-                    idx_del.append(i)
-
-            HR    = np.delete(HR, np.array(idx_del))
-            t_RR1 =  np.delete(t_RR, np.array(idx_del)+1)
-            t_RR2 =  np.delete(t_RR, np.array(idx_del))
-            t_PPG = t[idx_peaksPPG]
-
-            plt.figure()
-            plt.subplot(211)
-            plt.plot(t,ECG_aux)
-            plt.scatter(t_RR,ECG_aux[idx_peaksECG], c='r')
-            plt.title(nombres[k])
-            plt.subplot(212)
-            plt.plot(t,PPG_aux,'g')
-            plt.scatter(t_PPG,PPG_aux[idx_peaksPPG], c='y')
-
-            PPT1 = (t_PPG-t_RR2[:-1])
-            PPT2 = (t_RR1[1: ]-t_PPG)
-            try:
-                t_HR_cu.append(np.mean(HR))
-
-                if np.mean(PPT1)>np.mean(PPT2):
-                    t_PPT_cu.append(np.mean(PPT1))
-                else:
-                    t_PPT_cu.append(np.mean(PPT2))
-    #            plt.figure()
-    #            plt.plot(PPT1,'gx',label='PPG peak - previous RR peak ')
-    #            plt.plot(PPT2,'rx',label='subsequent RR peak - PPG peak')
-    #            plt.legend()
-            except:
-                t_HR_cu.append([])
-                t_PPT_cu.append([])
-
-        except:
-            t_HR_cu.append([])
-            t_PPT_cu.append([])
-
-        idx_a = j
-    t_HR.append(t_HR_cu)
-    t_PPT.append(t_PPT_cu)
+idx = np.ones(np.shape(HR_norm), dtype=bool)
+idx = np.where(abs(HR_norm)<2,idx,False)
+idx = np.where(abs(logPPT_norm)<2,idx,False)   
+idx = np.where(abs(DPB_norm)<2,idx,False)
+idx = np.where(abs(SBP_norm)<2,idx,False)
 
 
 
+X = np.transpose(np.array([HR_norm[idx], logPPT_norm[idx]]))
+y1  = np.array(t_DBP)[idx]
+y2  = np.array(t_SBP)[idx]
+
+plt.figure()
+sns.set_style("whitegrid")
+sns.jointplot(X[:,1], y2, kind ='kde')
+plt.xlabel('PPT') # Set text for the x axis
+plt.ylabel('Diastolic Blood Pressure')# Set text for y axi
 plt.show()
 
-#datos = np.genfromtxt('https://raw.githubusercontent.com/maortiz1/cuffofPD2/master/real-time-plot-microphone-kivy/11-9-11-17_ecg.txt',delimiter=',')
-#plt.figure()
-#plt.plot(datos)
-##plt.figure()
-##plt.plot(datos[:,1])
+
+
+regSBP = linear_model.LinearRegression()
+regDBP = linear_model.LinearRegression()
+
+#regSBP = SVR(kernel='linear', C=3)
+#regDBP = SVR(kernel='linear', C=3)
 #
-#b, a = scipy.signal.butter(1,[0.04,0.92],'bandpass')
-#x = datos[:-1]
-## ecg
-#y = filtfilt(b, a, x, method='gust')
-#
-## b, a = scipy.signal.butter(1,[0.01,0.04],'bandpass')
-## x = datos[:-1]
-## # ppg
-## y = filtfilt(b, a, x, method='gust')
-##
-## # b, a = scipy.signal.butter(1,0.01,'highpass')
-## y = filtfilt(y, a, x, method='gust')
-#plt.figure()
-#plt.plot(x,'r')
-#plt.plot(y,'g')
-#
-#
-#fs = 250
-#data_ECG = scale(x)
-#t = (np.arange(len(data_ECG))/fs)
-#idx_peaksECG = nk.bio_process(ecg = data_ECG, sampling_rate=125)['ECG']['R_Peaks']
-#t_RR = t[idx_peaksECG]
-#
-#plt.figure()
-#
-#plt.plot(t,data_ECG)
-#plt.scatter(t[idx_peaksECG],data_ECG[idx_peaksECG], c ='r')
-#
+#regDBP = AdaBoostRegressor(n_estimators=100)
+#regSBP = AdaBoostRegressor(n_estimators=100)
+#    
+#regDBP = DecisionTreeRegressor()
+#regSBP = DecisionTreeRegressor()
+
+kf = KFold(n_splits=5)
+kf.split(X)
+
+errorSBP = []
+errorDBP = []
+for train, test in kf.split(X):
+    X_train, X_test, y1_train, y1_test, y2_train, y2_test = X[train], X[test], y1[train], y1[test], y2[train], y2[test]
+
+    regSBP.fit(X_train, y1_train) 
+    regDBP.fit(X_train, y2_train) 
+
+    errorSBP.append(sqrt(mean_squared_error(y1_test, regSBP.predict(X_test))))
+    errorDBP.append(sqrt(mean_squared_error(y2_test, regDBP.predict(X_test))))
+
+
+print('Root Mean Squared Error SBP')
+print('mean',np.mean(errorSBP))
+print('Root Mean Squared Error DBP')
+print('mean',np.mean(errorDBP))
+
+
+
 #plt.show()
+#
+##datos = np.genfromtxt('https://raw.githubusercontent.com/maortiz1/cuffofPD2/master/real-time-plot-microphone-kivy/11-9-11-17_ecg.txt',delimiter=',')
+##plt.figure()
+##plt.plot(datos)
+###plt.figure()
+###plt.plot(datos[:,1])
+##
+##b, a = scipy.signal.butter(1,[0.04,0.92],'bandpass')
+##x = datos[:-1]
+### ecg
+##y = filtfilt(b, a, x, method='gust')
+##
+### b, a = scipy.signal.butter(1,[0.01,0.04],'bandpass')
+### x = datos[:-1]
+### # ppg
+### y = filtfilt(b, a, x, method='gust')
+###
+### # b, a = scipy.signal.butter(1,0.01,'highpass')
+### y = filtfilt(y, a, x, method='gust')
+##plt.figure()
+##plt.plot(x,'r')
+##plt.plot(y,'g')
+##
+##
+##fs = 250
+##data_ECG = scale(x)
+##t = (np.arange(len(data_ECG))/fs)
+##idx_peaksECG = nk.bio_process(ecg = data_ECG, sampling_rate=125)['ECG']['R_Peaks']
+##t_RR = t[idx_peaksECG]
+##
+##plt.figure()
+##
+##plt.plot(t,data_ECG)
+##plt.scatter(t[idx_peaksECG],data_ECG[idx_peaksECG], c ='r')
+##
+##plt.show()
--- real-time-plot-microphone-kivy/recortar.py
+++ real-time-plot-microphone-kivy/recortar.py
@@ -12,30 +12,28 @@ nombres = datos_t[::9,0]
 
 cortar = [150000, 50000, 22000, 105000, 115000, 46000, 64000, 43000, 60000, 70000, 80000, 88000, 72000, 5500, 22000, 46000, 13000, 29000, 65000]
 
-for i in range(nombres.shape[0]-1, nombres.shape[0]):
-    plt.figure()
-    ECG = np.genfromtxt('%s_ecg.txt'%nombres[i], delimiter = ',')
-    plt.subplot(211)
-    plt.plot(ECG[:-1])
-    ECG = ECG[cortar[i]:-1]
-    plt.plot(ECG)
-    file = open('%s_ecg_copy.txt'%(nombres[i]),'a+')
+for i in range(nombres.shape[0]):
+    print(nombres[i])
+    ECG = np.genfromtxt('%s_ecg_copy.txt'%nombres[i], delimiter = ',')
+    print(ECG.shape)
+#    ECG = ECG[cortar[i]:-1]
+#    print(ECG.shape)
+#    file = open('%s_ecg_copy.txt'%(nombres[i]),'a+')
+#
+#    for j in range(ECG.shape[0]):
+#        file.write(str( '%d \n'%ECG[j]))
 
-    for j in range(ECG.shape[0]):
-        file.write(str( '%d \n'%ECG[j]))
+#    file.close()
+#   
+    PPG = np.genfromtxt('%s_ppg_copy.txt'%nombres[i], delimiter = ',')
+    print(PPG.shape)
+#    PPG = PPG[cortar[i]:-1]
+#    print(PPG.shape)
 
-    file.close()
-    plt.subplot(212)
-    PPG = np.genfromtxt('%s_ppg.txt'%nombres[i], delimiter = ',')
-    plt.plot(PPG[:-1])
-    PPG = PPG[cortar[i]:-1]
-    plt.plot(PPG)
+#    file1 = open('%s_ppg_copy.txt'%(nombres[i]),'a+')
+#
+#    for j in range(PPG.shape[0]):
+#        file1.write(str( '%d \n'%PPG[j]))
+#
+#    file1.close()
 
-    file1 = open('%s_ppg_copy.txt'%(nombres[i]),'a+')
-
-    for j in range(PPG.shape[0]):
-        file1.write(str( '%d \n'%PPG[j]))
-
-    file1.close()
-
-plt.show()
